c98562ddfdea14334095881aed492bb41e192e07
서동욱 <seonaru@yanolja.com>
2025-09-23 15:46:01 +0900

    10

---
+++ python_template.py
@@ -0,0 +1,16 @@
+"""
+백준 문제 풀이 템플릿 - Python
+문제 번호: 
+문제 제목: 
+난이도: 
+"""
+
+import sys
+input = sys.stdin.readline
+
+def solve():
+    # 여기에 문제 풀이 로직을 작성하세요
+    pass
+
+if __name__ == "__main__":
+    solve()
