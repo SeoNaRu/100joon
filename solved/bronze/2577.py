1c440eedc309851868d0b92ebdb23232278b64a2
SeoNaRu <tjehddnr852@gmail.com>
2025-09-26 16:37:30 +0900

    100

---
+++ solved/bronze/2577.py
@@ -0,0 +1,13 @@
+# A = int(input())
+# B = int(input())
+# C = int(input())
+
+# result = str(A * B * C)
+# for i in range(10):
+#     print(result.count(str(i)))
+
+
+# print(result)
+
+print('111'.count('1'))
+
